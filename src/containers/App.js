5a95cac20d859eb582bfde4dd38d51b4c3ba919b
Juan Mondragon <jhmndrgn56@gmail.com>
2021-02-20 14:33:30 -0600

    this is the official build

--- src/containers/App.js
+++ src/containers/App.js
@@ -18,7 +18,7 @@ class App extends Component {
 
 
     componentDidMount() {
-        fetch('http://jsonplaceholder.typicode.com/users')
+        fetch('https://jsonplaceholder.typicode.com/users')
             .then(response=> response.json())
             .then(users => this.setState({ robots: users }));
         
@@ -35,7 +35,7 @@ class App extends Component {
             return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         });
         return !robots.length ? 
-            <h1>Loading1</h1> :
+            <h1>Loading</h1> :
             (
             <div className='tc'>
                 <h1 className='f2'>RoboFriends</h1>
